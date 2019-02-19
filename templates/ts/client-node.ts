94dd9af9f5b59c1b624d754075e00ebd582a522a
Roey Berman <roey.berman@gmail.com>
2019-02-19 17:49:12 +0200

    Implement CR comments

--- templates/ts/client-node.ts
+++ templates/ts/client-node.ts
@@ -20,11 +20,15 @@ import {
   {{/bypassTypes}}
 } from './interfaces';
 
+export class TimeoutError extends Error {
+}
+
 export {
   ValidationError,
 };
 
 export interface Options extends Pick<RequestInit, 'agent' | 'redirect' | 'follow' | 'compress'> {
+  fetchImplementation?: typeof fetch;
   timeoutMs?: number;
   headers?: Record<string, string>;
 }
@@ -82,40 +86,47 @@ export class {{name}}Client {
       delete mergedOptions.timeoutMs;
     }
 
-    const response = await fetch(`${this.serverUrl}/{{name}}`, {
-      ...mergedOptions,
-      headers: {
-        ...mergedOptions.headers,
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(body),
-      method: 'POST',
-    });
-    const isJSON = (response.headers.get('content-type') || '').startsWith('application/json');
-    if (response.status >= 200 && response.status < 300) {
-      const validator = this.validators.{{{name}}};
-      const wrapped = { returns: isJSON ? await response.json() : undefined }; // wrapped for coersion
-      if (!validator(wrapped)) {
-        throw new ValidationError('Failed to validate response', validator.errors);
+    try {
+      const response = await (mergedOptions.fetchImplementation || fetch)(`${this.serverUrl}/{{name}}`, {
+        ...mergedOptions,
+        headers: {
+          ...mergedOptions.headers,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+        method: 'POST',
+      });
+      const isJSON = (response.headers.get('content-type') || '').startsWith('application/json');
+      if (response.status >= 200 && response.status < 300) {
+        const validator = this.validators.{{{name}}};
+        const wrapped = { returns: isJSON ? await response.json() : undefined }; // wrapped for coersion
+        if (!validator(wrapped)) {
+          throw new ValidationError('Failed to validate response', validator.errors);
+        }
+        return wrapped.returns as {{{returnType}}};
+      } else if (!isJSON) {
+        throw new Error(`${response.status} - ${response.statusText}`);
+      } else if (response.status === 400) {
+        const body = await response.json();
+        if (body.name === 'ValidationError') {
+          throw new ValidationError(body.message, body.errors);
+        }
+      } else if (response.status === 500) {
+        const body = await response.json();
+        {{#throws}}
+        if (body.name === '{{.}}') {
+          throw new {{.}}(body.message);
+        }
+        {{/throws}}
+        throw new InternalServerError(body.message);
       }
-      return wrapped.returns as {{{returnType}}};
-    } else if (!isJSON) {
       throw new Error(`${response.status} - ${response.statusText}`);
-    } else if (response.status === 400) {
-      const body = await response.json();
-      if (body.name === 'ValidationError') {
-        throw new ValidationError(body.message, body.errors);
+    } catch (err) {
+      if (err.message === 'The user aborted a request.') {
+        throw new TimeoutError('Request aborted due to timeout on method "{{{name}}}"');
       }
-    } else if (response.status === 500) {
-      const body = await response.json();
-      {{#throws}}
-      if (body.name === '{{.}}') {
-        throw new {{.}}(body.message);
-      }
-      {{/throws}}
-      throw new InternalServerError(body.message);
+      throw err;
     }
-    throw new Error(`${response.status} - ${response.statusText}`);
   }
   {{/methods}}
 }
