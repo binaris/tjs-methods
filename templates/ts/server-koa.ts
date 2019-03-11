ba114e76474b3ca7ab5848837652d3c26555867b
Vladimir Zoubritsky <vladimir@binaris.com>
2019-03-11 12:25:07 +0200

    CR: Test context being stored in koa state

--- templates/ts/server-koa.ts
+++ templates/ts/server-koa.ts
@@ -91,8 +91,10 @@ export class {{name}}Server {
   public constructor(
     protected readonly handler: {{name}}Handler,
     stackTraceInError = false,
+    initializeApp: (app: Koa) => void = () => {}
   ) {
     this.app = new Koa();
+    initializeApp(this.app);
     this.router = new {{name}}Router(handler, stackTraceInError);
 
     this.app.use(this.router.koaRouter.routes());
