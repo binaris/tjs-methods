eb3607936908f6b1af7f72ff832167fe3ddfeba6
Roey Berman <roey.berman@gmail.com>
2019-05-22 09:49:00 +0300

    Add signal to spawn helper

--- src/utils.ts
+++ src/utils.ts
@@ -6,12 +6,12 @@ export const spawn = (
   options?: SpawnOptions
 ): Promise<number> => new Promise((resolve, reject) => {
   const child = origSpawn(command, args, options);
-  child.on('close', (code: number) => {
+  child.on('close', (code, signal) => {
     if (code === 0) {
       resolve(code);
     } else {
       const fullCommand = [command].concat(args || []).join(' ');
-      reject(new Error(`command: '${fullCommand}' failed process failed with exit code: ${code}`));
+      reject(new Error(`command: '${fullCommand}' failed process failed with exit code: ${code}, signal: ${signal}`));
     }
   });
   child.on('error', reject);
